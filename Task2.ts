0a196e2543a468c4df62df572a08c033f1490f2f
Nazir <91749601+Nazir-Rizwan@users.noreply.github.com>
2023-08-03 06:03:08 -0700

    files to folder

--- Task2.ts
+++
@@ -1,4 +0,0 @@
-let personal_message:string='Hello Nazir Would you like to learn some python today?';
-console.log(`${personal_message}`)
-
-
---
+++ Task2/Task2.ts
@@ -0,0 +1,4 @@
+let personal_message:string='Hello Nazir Would you like to learn some python today?';
+console.log(`${personal_message}`)
+
+
