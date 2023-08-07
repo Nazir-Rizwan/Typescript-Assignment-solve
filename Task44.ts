fda480f8c6d1aece5c5423fedf8443d1cc080612
Nazir <91749601+Nazir-Rizwan@users.noreply.github.com>
2023-08-07 11:45:53 -0700

    update Task44.ts

--- Task44.ts
+++ Task44.ts
@@ -4,7 +4,7 @@ const sandwitchlist=function(...list:string[]):string[]{
     return list.sort();
 }
 
-console.log(`${sandwitchlist(...sandwitch)} `);
+console.log(`${sandwitchlist(...sandwitch)}`);
 console.log(sandwitchlist("seafood sandwitch","roast beef sandwitch","ham sandwitch"))
 console.log(sandwitchlist("Nutella Sandwich","Grilled Chicken Sandwich"))
 
