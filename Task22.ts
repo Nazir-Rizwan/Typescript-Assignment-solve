09ada2578cdc41a2d232e8fdbc9a69776ea85708
Nazir <91749601+Nazir-Rizwan@users.noreply.github.com>
2023-08-11 03:09:40 -0700

    index error

---
+++ Task22.ts
@@ -0,0 +1,10 @@
+//  index out of range 
+const languages:string[]=["c++","java","python"]
+
+// Total 3 index values 
+//  if use -1 values then 
+
+console.log(languages.indexOf('python'))
+
+// so last index 2 
+console.log(`${languages[3]}`)
